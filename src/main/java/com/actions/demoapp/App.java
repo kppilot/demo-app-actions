db562af9755ef61a4ae2dd29b6c56daa019556d0
Asim Sheikh <asim.sheikh@syncron.com>
2023-03-10 10:56:32 +0530

    first

---
+++ src/main/java/com/actions/demoapp/App.java
@@ -0,0 +1,13 @@
+package com.actions.demoapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
