31c3d92069199401e24a7eb7f79f50da9eea7f59
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-21 17:05:22 +0530

    done

--- deploy/providers.tf
+++ deploy/providers.tf
@@ -11,11 +11,9 @@ locals {
   app_name = "ecs_deploy_gh_actions"
   gh_org_name = "sheikhasim"
 
-locals {
-  db_creds = jsondecode(
+db_creds = jsondecode(
     data.aws_secretsmanager_secret_version.creds.secret_string
   )
-}
 
 }
 
