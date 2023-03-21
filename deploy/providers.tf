7bf304c6389cad5cf5d863b831fb8657808fde1a
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-21 16:39:10 +0530

    terraform

---
+++ deploy/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.59.0"
+    }
+  }
+}
+
+locals {
+  app_name = "ecs_deploy_gh_actions"
+  gh_org_name = "sheikhasim"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      application = local.app_name
+    }
+  }
+}
--- deploy/terraform.tfvars
+++ deploy/terraform.tfvars
@@ -1,1 +1,3 @@
-image_id = "v2.1.0"
+aws_access_key = ${{secrets.AWS_ACCESS_KEY_ID}} 
+aws_secret_key = ${{secrets.AWS_SECRET_ACCESS_KEY}} 
+aws_region = "ap-northeast-1"
