8e9d36ffec3d6e073035954a61fed7944ccad975
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-22 10:25:27 +0530

    test

--- deploy/providers.tf
+++ deploy/providers.tf
@@ -7,24 +7,17 @@ terraform {
   }
 }
 
-data "aws_secretsmanager_secret_version" "creds"{
-  secret_id = "creds"
-}
 locals {
   app_name = "ecs_deploy_gh_actions"
   gh_org_name = "sheikhasim"
 
-creds = jsondecode(
-    data.aws_secretsmanager_secret_version.creds.secret_string
-  )
-
 }
 
-provider "aws"{
-  aws_access_key= locals.creds.AWS_ACCESS_KEY
-  aws_secret_key = locals.creds.AWS_SECRET_KEY
-  region = var.aws_region
+provider "aws" {
+  region = "ap-northeast-1"
   default_tags {
-    application = local.app_name
+    tags = {
+      application = local.app_name
+    }
   }
 }
