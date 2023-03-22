c74ae1adf4295084d99865a7445f128bcd880ff4
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-22 12:46:33 +0530

    service

--- deploy/ecs.tf
+++ deploy/ecs.tf
@@ -90,6 +90,25 @@ resource "aws_lb_listener_rule" "myservice" {
   }
 }
 
+resource "aws_ecs_service" "service" {
+  name = "app_service"
+  cluster                = aws_ecs_cluster.this.arn
+  launch_type            = "FARGATE"
+  enable_execute_command = true
+
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
+  desired_count                      = 1
+  task_definition                    = aws_ecs_task_definition.td.arn
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups  = [aws_security_group.app.id]
+    subnets          = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  }
+}
+
+
 ####################################################
 # ECR Repository
 ####################################################
