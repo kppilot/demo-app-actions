d561a5c328f984b0be7b34425d7b6804de959c22
Asim Sheikh <asim.sheikh@syncron.com>
2023-03-27 22:32:28 +0530

    added

--- terraform/commands/pull_push.sh
+++ terraform/commands/pull_push.sh
@@ -3,8 +3,8 @@
 SOURCE=$1
 DEST=$2
 
-bash curl -sSL https://get.docker.com/ | sudo sh
-bash docker login -u sheikh111 -p Play4m33@
-bash docker pull $SOURCE
-bash docker tag $SOURCE $DEST
-bash docker push $DEST
+sudo apt-get install docker-ce
+docker login -u sheikh111 -p Play4m33@
+docker pull $SOURCE
+docker tag $SOURCE $DEST
+docker push $DEST
