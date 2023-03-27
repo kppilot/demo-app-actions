19460081d7be8c1f3ec254b2a3ed5e116927ffea
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-27 20:47:01 +0530

    Update pull_push.sh

--- terraform/commands/pull_push.sh
+++ terraform/commands/pull_push.sh
@@ -3,6 +3,5 @@
 SOURCE=$1
 DEST=$2
 
-docker pull $SOURCE
-docker tag $SOURCE $DEST
-docker push $DEST
+docker pull sheikh111/demo-actions-repo:v23.0.1
+docker tag sheikhasim111/demo-actions-repo:v23.0.1 abc:1.0
