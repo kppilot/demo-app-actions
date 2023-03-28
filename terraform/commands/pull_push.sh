bd483d5b9be402ddcdc3fddc24395a1df3ef7368
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-28 16:39:24 +0530

    sudo

--- terraform/commands/pull_push.sh
+++ terraform/commands/pull_push.sh
@@ -4,7 +4,13 @@ SOURCE=$1
 DEST=$2
 
 whoami
-sudo docker --version
-sudo docker pull $SOURCE
-sudo docker tag $SOURCE $DEST
-sudo docker push $DEST
+apt install sudo
+sudo -h
+
+yum install sudo
+sudo -h
+
+#sudo docker --version
+#sudo docker pull $SOURCE
+#sudo docker tag $SOURCE $DEST
+#sudo docker push $DEST
