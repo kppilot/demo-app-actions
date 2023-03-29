88c8b0f28d1ea81e56f39492f4f381edd13a9b54
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-29 16:25:50 +0530

    debug login

--- terraform/commands/pull_push.sh
+++ terraform/commands/pull_push.sh
@@ -7,5 +7,5 @@ DEST=$2
 sudo usermod -aG docker runner
 sudo docker pull $SOURCE
 sudo docker tag $SOURCE $DEST
-aws ecr get-login-password --region ap-northeast-2 --profile user1|  docker login --username AWS --password-stdin 628034938073.dkr.ecr.ap-northeast-2.amazonaws.com
+aws ecr get-login-password --region ap-northeast-2 --profile user1 --debug|  docker login --username AWS --password-stdin 628034938073.dkr.ecr.ap-northeast-2.amazonaws.com
 sudo docker push $DEST
