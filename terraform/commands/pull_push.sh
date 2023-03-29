ebbfdd133d4ddb7f54ecacbd3113378cd0f5f3f5
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-29 16:58:20 +0530

    cd

--- terraform/commands/pull_push.sh
+++ terraform/commands/pull_push.sh
@@ -7,7 +7,7 @@ DEST=$2
 echo $SOURCE
 echo $DEST
 sudo usermod -aG docker runner
-sudo docker pull $SOURCE
-#sudo docker tag $SOURCE 628034938073.dkr.ecr.ap-northeast-2.amazonaws.com/ecs_deploy_gh_actions-myservice:v23.0.1
-aws ecr get-login-password --region ap-northeast-2 --profile user1 --debug|  docker login --username AWS --password-stdin 628034938073.dkr.ecr.ap-northeast-2.amazonaws.com
+sudo docker pull $SOURCE 
+sudo docker tag $SOURCE 628034938073.dkr.ecr.ap-northeast-2.amazonaws.com/ecs_deploy_gh_actions-myservice:v23.0.1
+aws ecr get-login-password --region ap-northeast-2 |  docker login --username AWS --password-stdin 628034938073.dkr.ecr.ap-northeast-2.amazonaws.com
 sudo docker push 628034938073.dkr.ecr.ap-northeast-2.amazonaws.com/ecs_deploy_gh_actions-myservice:v23.0.1
