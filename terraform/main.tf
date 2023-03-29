ccddae53827957670b501f1e718aa350cfb2dbf3
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-29 10:33:43 +0530

    s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
 terraform {
-    backend "s3" {
-    bucket = "demoapp_bucket"
-    key    = "state/terraform.tfstate"
-    region = "ap-northeast-2"
-    profile = "test1"
-  }
+  backend "s3" {}
 }
