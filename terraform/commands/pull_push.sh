c928eb9d0970c56133bbb4a1e9a0568c323086e6
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-30 15:16:51 +0530

    cd

--- terraform/commands/pull_push.sh
+++ terraform/commands/pull_push.sh
@@ -14,7 +14,7 @@ aws sts get-caller-identity
 sudo docker pull $SOURCE 
 sudo docker tag $SOURCE 628034938073.dkr.ecr.ap-northeast-2.amazonaws.com/ecs_deploy_gh_actions-myservice:v23.0.1
 #echo $(aws ecr get-login-password --region ap-northeast-2)
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 628034938073.dkr.ecr.ap-northeast-2.amazonaws.com
+#aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 628034938073.dkr.ecr.ap-northeast-2.amazonaws.com
 aws sts get-caller-identity
 cat ~/.docker/config.json
 cat ~/.aws/credentials
