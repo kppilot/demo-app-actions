e62dcbe999207eb37c6f44c90d86a3f1c32d11d1
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2023-03-30 14:39:21 +0530

    cd

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -15,8 +15,8 @@ locals {
 
 provider "aws" {
   region = var.aws_region
-  assume_role_with_web_identity {
-    role_arn = "arn:aws:iam::628034938073:role/github_actions_role"
+  #assume_role_with_web_identity {
+    #role_arn = "arn:aws:iam::628034938073:role/github_actions_role"
   }
  
   default_tags {
